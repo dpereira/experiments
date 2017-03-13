533ceb991d71c149740579d3cd2df9aa137ceff6
Diego Pereira <rockin.jack@gmail.com>
2017-03-12 22:21:18 -0300

    fix: dedicated connection pool for blocking ops
    brpop/blpops should not be shared between coroutines,
    otherwise they will impose contention on one another

--- async_brpop/producer.py
+++ async_brpop/producer.py
@@ -5,33 +5,57 @@ async def produce(client, count):
     num_tasks = await client.rpush('tasks', 'task_%s' % count)
     print('Sent %s, %s total tasks in queue' % (count, num_tasks))
 
-async def get_result(client, count):
-    task = 'task_%s' % count
-    print('Waiting for %s' % task)
-    key, task = await client.blpop(task)
-    print('Received result for %s' % task)
-
-async def run(loop):
-    clients = []
-    for i in range(4):
-        clients.append(await aioredis.create_redis(
+async def get_result(count, loop, readers=[]):
+    print('%s total readers active' % len(readers))
+    try:
+        reader = readers.pop()
+    except IndexError:
+        reader = await aioredis.create_redis(
             ('localhost', 6379),
             loop=loop
-        ) )
-        
-    tasks = []
-    objects = []
-    for i in range(7):
-        await produce(clients[i % 4], i)
+        )
+    task = 'task_%s' % count
+    print('Waiting for %s' % task)
+    key, task = await reader.blpop(task, timeout=20)
+    print('Received result for %s' % task)
+    readers.append(reader)
 
-    for i in range(7):
-        f = get_result(clients[i % 4], i)
-        objects.append(f)
-        tasks.append(asyncio.ensure_future(f))
+async def run(loop):
+    client = await aioredis.create_redis(
+        ('localhost', 6379),
+        loop=loop
+    )
+    readers = []
+       
+   
+    wtasks = []
+    rtasks = []
+
+    num_messages = 10000
+
+    for i in range(num_messages):
+        f =  produce(client, i)
+        wtasks.append(asyncio.ensure_future(f))
+
+        f = get_result(i, loop, readers)
+        rtasks.append(asyncio.ensure_future(f))
+
+        if len(rtasks) >= 10:
+            print('%s max rtasks reached, syncing' % (len(rtasks)))
+            print('Free readers PRIOR: %s' % len(readers))
+            rtasks = list(filter(lambda f: not f.done(), rtasks))
+            if rtasks:
+                await asyncio.wait(rtasks, return_when=asyncio.FIRST_COMPLETED)
+            print('Free readers AFTER: %s' % len(readers))
 
     print('All sent.')
-    await asyncio.wait(tasks)
+    await asyncio.wait(wtasks + rtasks)
     print('All done.')
+    print('Had to spawn %s readers' % len(readers))
+
+    for r in readers + [client]:
+        r.close()
+        await r.wait_closed()
 
 loop = asyncio.get_event_loop()
 loop.run_until_complete(run(loop))
