02cc4344d5f9f4b5819d7ad5fa0336dd344fcd4a
Diego Pereira <diegop@pm.me>
2022-08-09 23:32:18 -0300

    chore: add basic lora experiments

---
+++ arduino/lora/lora_sender_no_serial/lora_sender_no_serial.ino
@@ -0,0 +1,21 @@
+#include <LoRa.h>
+
+int counter = 0;
+
+void setup() {
+  if (!LoRa.begin(868E6)) {
+    while (1);
+  }
+}
+
+void loop() {
+  // send packet
+  LoRa.beginPacket();
+  LoRa.print("hello ");
+  LoRa.print(counter);
+  LoRa.endPacket();
+
+  counter++;
+
+  delay(5000);
+}
